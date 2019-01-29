5760ad006453512a9f6d0868ac1331f24f1a412d
Maymomo <3466365807@qq.com>
2019-01-29 21:24:48 +0800

    add problem search

--- src/pages/problems/index.js
+++ src/pages/problems/index.js
@@ -1,23 +1,28 @@
 import React, {Component} from 'react';
 import {connect} from 'dva';
-import {Col, Row, Table} from 'antd';
+import {Col, Row, Table, Input, Layout} from 'antd';
 import axios from 'axios';
 import {PROBLEMS_URL} from "@/api/api";
 
+const Search = Input.Search;
+
 const columns = [{
-  title: 'ID',
+  title: 'PID',
   dataIndex: 'pid',
-  sorter: true,
+  sorter: function (row_1, row_2) {
+    return row_1.pid > row_2.pid;
+  },
+  sortDirections: ['descend', 'ascend'],
 }, {
   title: 'Name',
   dataIndex: 'name',
 }, {
-  title: 'submit',
-  dataIndex: 'submit',
-}, {
-  title: 'solved',
+  title: 'Solved',
   dataIndex: 'solved',
-},
+}, {
+  title: 'Rate',
+  dataIndex: 'rate',
+}
 ];
 
 class ProblemsTable extends Component {
@@ -27,40 +32,62 @@ class ProblemsTable extends Component {
     this.fetch({page: 1});
   }
 
-  fetch = (params= {}) => {
+  fetch = (params = {}) => {
     this.setState({loading: true});
     axios.get(PROBLEMS_URL, {params: {page: params.page, capacity: 15}}
     ).then(response => {
       if (response.data.hasOwnProperty("errCode") && response.data.errCode === 0) {
         let msg = window.atob(response.data.message);
         msg = JSON.parse(msg);
+        for (var i = 0; i < msg.data.length; i++) {
+          let rate = 'N/A';
+          if (msg.data[i].submit > 0) {
+            rate = (100.0 * msg.data[i].solved / msg.data[i].submit).toFixed(2);
+            rate = rate + "%";
+          }
+          msg.data[i].rate = rate;
+        }
+
         this.setState({data: msg.data, loading: false, pagination: {total: msg.max_pages + 1}});
       } else {
         let msg = window.atob(response.data.message);
         msg = JSON.parse(msg);
         alert(msg);
       }
-    }).catch((e) =>
-    {
-      console.log(e);
-    }
-  )
+    }).catch((e) => {
+        console.log(e);
+      }
+    )
   };
   handleTableChange = (pagination, filters, sorter) => {
+    console.log(pagination, filters, sorter);
     this.fetch({page: pagination.current});
   };
 
   render() {
     return (
-      <Table
-        style={{background: "white"}}
-        bordered={true}
-        columns={columns}
-        dataSource={this.state.data}
-        pagination={this.state.pagination}
-        loading={this.state.loading}
-        onChange={this.handleTableChange}>
-      </Table>
+      <Layout style={{background: "white"}}>
+        <Row style={{height: "50px", lineHeight: "50px"}}>
+          <Col style={{float: "left", paddingLeft: "20px"}}><h3>Problems</h3></Col>
+          <Col style={{float: "right", marginRight: "20px"}}>
+            <Search
+              placeholder="Search Problem"
+              onSearch={value => console.log(value)}
+              enterButton
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Table
+            bordered={true}
+            columns={columns}
+            dataSource={this.state.data}
+            pagination={this.state.pagination}
+            loading={this.state.loading}
+            onChange={this.handleTableChange}>
+          </Table>
+        </Row>
+      </Layout>
     );
   }
 }
@@ -71,9 +98,7 @@ function Problems(state) {
     <Row>
       <Col md={3}/>
       <Col md={18}>
-        <div>
-          <ProblemsTable/>
-        </div>
+        <ProblemsTable/>
       </Col>
       <Col md={3}/>
     </Row>
